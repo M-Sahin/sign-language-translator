22949488cab3b744175956cf1c0062f26c6828f5
mehmet70 <mehmetemin.balci@live.nl>
2022-03-22 20:02:25 +0100

    translation page
    translation page, that shows the images of the typed letters

--- src/components/translation/translationView.jsx
+++ src/components/translation/translationView.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
 function Translation() {
+
+    const [letters, setLetters] = React.useState([])
+
+    let textInput = React.createRef();
+
+    const translate = event =>{
+
+        let currentText = textInput.current.value
+        let splitText = currentText.replace(/ /g, "").split('')
+        setLetters(splitText)
+    }
+
     return (
         <div>
-            <h1>This is the Translation page</h1>
+            <h1>Translation</h1>
+            <div className="form-group">
+                <label htmlFor="exampleFormControlTextarea1">Fill in a word to translate</label>
+                <textarea ref={textInput} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+            <div id = 'handSigns'>
+                {letters.map((letter)=>{
+                    return <img  src={require('../../assets/handSigns/' + letter + '.png')} /> 
+                }
+
+                )}
+            </div>
+            <button onClick={ translate } type="button" className="btn btn-primary">Translate</button>
         </div>
     )
 }
